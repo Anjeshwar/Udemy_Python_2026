245b388c101ff99b5a66e7568bce461f3cfe5cc1
Anjeshwara chary <mulkallaanjeshwar1@gmail.com>
2026-06-09 23:10:57 +0530

    Day4 Lists, Tuples, Sets, Dictionaries

--- Section 1/day4.py
+++ Section 1/day4.py
@@ -80,3 +80,56 @@ allCars = RichEndCars.union(NormalCars)                     # give both the sets
 print(allCars)
 
 
+print("\n-------------\n-------------\n")
+
+
+# Dictionaries - Key:Value pair - No duplicate keys, ordered.
+friend_ages = {"Anj":25, "Mohan":49, "Sai":24}  # {key:value}
+print(friend_ages)
+print(friend_ages["Anj"])
+print(friend_ages["Mohan"])
+print(friend_ages["Sai"])
+
+friend_ages["Mac"] = 24     # adding a key:value into a dictionary
+print(friend_ages)
+
+friend_ages["Sai"] = 23     # modifying/changing existing value in a dictionary
+print(friend_ages)
+
+print("--------------------\n")
+
+# dictionary inside a tuple
+friends_of_me = (
+    {"Name": "Anjesh", "Age": 25},
+    {"Name": "Sai", "Age":24},
+    {"Name": "Mohan", "Age": 49}
+)
+print(friends_of_me)
+
+print(friends_of_me[0]["Name"])
+print(friends_of_me[1]["Name"])
+print(friends_of_me[1]["Age"])
+print(friends_of_me[2])
+
+
+# Converting list of tuples into a dictionary
+cars_base_price = [("Tata", 1000000), ("Audi", 5000000), ("Mercedes", 4000000)]
+print(cars_base_price)
+
+car_prices = dict(cars_base_price)  # dict
+print(car_prices)
+
+
+print("----------------------\n")
+
+
+# sum, len
+marks = [99, 76, 91, 88, 79, 98]
+total = sum(marks)
+subjects = len(marks)
+
+print(total)
+print(subjects)
+
+average = total/subjects
+print(average)
---
+++ tempCodeRunnerFile.javascript
@@ -0,0 +1,6 @@
+my_friends = {
+    'Jose': {'last_seen': 6},
+    'Rolf': {'surname': 'Smith'},
+    'Anne': 6
+}
+print(my_friends['Jose']['last_seen'])
